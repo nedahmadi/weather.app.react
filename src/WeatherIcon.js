58b8ec5b676ff51a553b7eb086063dfa79ab1bee
nedahmadi <nedahmadi83@gmail.com>
2022-03-01 03:00:22 +0330

    EDITTED ICONS

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 export default function WeatherIcon(props) {
+  console.log(props.code);
   const codeMapping = {
-    "01d": "CLEAR_DAY",
-    "01n": "CLEAR_NIGH",
+    "01d": "CLAER_DAY",
+    "01n": "CLEAR_NIGHT",
     "02d": "PARTLY_CLOUDY_DAY",
-    "02n": "PARTLY_CLOUDY_NIGH",
-    "03d": "scattered clouds_DAY",
-    "03n": "scattered clouds_NIGH",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "PARTLY_CLOUDY_DAY",
+    "03n": "PARTLY_CLOUDY_NIGHT",
     "04d": "CLOUDY",
     "04n": "CLOUDY",
     "09d": "RAIN",
