b980c6f2ef34fd82354cb0a5f73a6cfbd980c390
nedahmadi <nedahmadi83@gmail.com>
2022-03-05 17:47:14 +0330

    complete forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 
 import "./WeatherForecast.css";
@@ -7,6 +7,10 @@ import axios from "axios";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -24,6 +28,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
@@ -31,8 +37,8 @@ export default function WeatherForecast(props) {
     );
   } else {
     let apiKey = "d2ced9a51dfc32ccb0ae22ecfcd93888";
-    let latitude = props.coordinates.lon;
-    let longitude = props.coordinates.lat;
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?
   lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
